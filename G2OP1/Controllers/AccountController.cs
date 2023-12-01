9b34c8956f8fa42896630d60d81d6d31d44db8d4
LojainHassan <lojainhteabat@gmail.com>
2023-12-01 17:25:27 +0300

    Add checking isActive

--- G2OP1/Controllers/AccountController.cs
+++ G2OP1/Controllers/AccountController.cs
@@ -9,7 +9,7 @@ namespace G2OP1.Controllers
         {
             new User{Id=1 , UserName="admin" , PassWord="admin",Email="admin@io" ,IsActive=true},
             new User{Id=2 , UserName="lojain" , PassWord="123",Email="lojain@io" ,IsActive=true},
-            new User{Id=3 , UserName="anas" , PassWord="123",Email="anas@io" ,IsActive=true}
+            new User{Id=3 , UserName="anas" , PassWord="123",Email="anas@io" ,IsActive=false}
         };
 
         [HttpGet]
@@ -21,12 +21,21 @@ namespace G2OP1.Controllers
         [HttpPost]
         public IActionResult Login(User user)
         {
+            
             if(UserList.Any(u=>u.UserName == user.UserName))
             {
                 if(UserList.Any(u => u.PassWord == user.PassWord))
                 {
                     ViewBag.LoginStatus = "Successfully Login";
+                    if(UserList.Where(u=>u.UserName == user.UserName).SingleOrDefault().IsActive == false)
+                    {
+                        ViewBag.LoginStatus = "Your Account is Locked";
+                        return View();
+                    }
+
                     return RedirectToAction("Index", "StudentInformation");
+
+                    
 
                 }
                 else
@@ -62,7 +71,10 @@ namespace G2OP1.Controllers
         [HttpPost]
         public IActionResult Register(User user)
         {
+            user.Id=UserList.Count()+1;
+            user.IsActive = true;
             UserList.Add(user);
+            
             return RedirectToAction("Login");
         }
     }
